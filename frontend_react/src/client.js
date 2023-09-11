b8c009c3b0440428a8e51038c8ed4d272bef1cf1
Sherwin <94516340+Smontan@users.noreply.github.com>
2023-09-11 15:33:41 +0900

    update the client file

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -7,6 +7,7 @@ export const client = createClient({
   apiVersion: "2023-09-06",
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true
 });
 
 const builder = imageUrlBuilder(client);
